84d7f87c767a0a4e709e048b14af0634afca897e
4nth0ny1 <acatullo4@gmail.com>
2023-06-30 11:28:00 -0500

    added all styles and html with hardcoded data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import words from "./wordlist.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
+
 function App() {
-  return <h1>hangman react typescript</h1>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>lose win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
